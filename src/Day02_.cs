ffe9c42f3657d154e2663295cf7eec462177dcea
Bailey Belvis <baileyb622@gmail.com>
2022-12-01 19:11:37 -0600

    day 01

---
+++ src/Day02_.cs
@@ -0,0 +1,62 @@
+﻿namespace AdventOfCode;
+
+/// <summary>
+///
+/// </summary>
+public class Day02_
+{
+	private static readonly string Input = "";
+
+	[Test]
+	public void Puzzle_01()
+	{
+		var lines = Input.GetCleanedLines();
+
+		var maxCalories = 0;
+		var currentElfCalorieCount = 0;
+
+		foreach (var line in lines)
+		{
+			if (int.TryParse(line, out var calorie))
+			{
+				currentElfCalorieCount += calorie;
+			}
+			else
+			{
+				if (currentElfCalorieCount > maxCalories)
+					maxCalories = currentElfCalorieCount;
+
+				currentElfCalorieCount = 0;
+			}
+		}
+
+		TestContext.Out.WriteLine($"Answer: {maxCalories}");
+	}
+
+	[Test]
+	public void Puzzle_02()
+	{
+		var lines = Input.GetCleanedLines();
+
+		var currentElfCalorieCount = 0;
+		var calories = new List<int>();
+
+		foreach (var line in lines)
+		{
+			if (int.TryParse(line, out var calorie))
+			{
+				currentElfCalorieCount += calorie;
+			}
+			else
+			{
+				calories.Add(currentElfCalorieCount);
+				currentElfCalorieCount = 0;
+			}
+		}
+
+		calories.Sort();
+		var answer = calories.TakeLast(3).Sum();
+
+		TestContext.Out.WriteLine($"Answer: {answer}");
+	}
+}
---
+++ src/Helpers.cs
@@ -0,0 +1,10 @@
+﻿namespace AdventOfCode;
+
+internal static class Helpers
+{
+	public static string[] GetCleanedLines(this string input)
+	{
+		return input.Replace("\r", String.Empty)
+					 .Split("\n", StringSplitOptions.TrimEntries);
+	}
+}
